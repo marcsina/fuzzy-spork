e1bd14cfd7105031983c6c413d36a07d48905a1e
Christopher G <zeus1400@googlemail.com>
2018-05-25 17:27:21 +0200

    edit conn

--- php/conn.php
+++ php/conn.php
@@ -1,7 +1,7 @@
 <?php
 
 //Datenbankverbindung aufbauen
-$mysqli = new mysqli("http://zeus1400.bplaced.net/", "zeus1400_root", "dicks1400", "zeus1400_Fuzzy");
+$mysqli = new mysqli("localhost", "zeus1400_root", "dicks1400", "zeus1400_Fuzzy");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
